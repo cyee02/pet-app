6e0fd9252a19a13f1cc6fa56486cd5acc4a3df17
cyee02 <chiazy94@gmail.com>
2021-11-16 15:26:15 +0800

    Message automatically scrolls to bottom on load

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -24,9 +24,8 @@ const Chat = ({myProfile}) =>{
   var userList
 
   useEffect(() => {
-    getProfiles(conversation.users)
     scrollToBottom()
-  }, [messages, conversation]);
+  });
 
   if (!myProfile) {
     history.push('/signin')
@@ -45,7 +44,6 @@ const Chat = ({myProfile}) =>{
       setText("");
     }
   }
-
   return(
     <Grid container spacing={2} style={{padding: "1rem"}}>
       <Grid item xs={3}>
@@ -59,7 +57,9 @@ const Chat = ({myProfile}) =>{
               : conversation.users.filter((user)=> user!==myProfile.username).join(", ")
             var users = conversation.users.filter(user => user!==myProfile.username).join(', ')
             return (
-              <ListItemButton key={conversation.conversationId} onClick={() => setConversation(conversation)}>
+              <ListItemButton key={conversation.conversationId} onClick={() => {
+                setConversation(conversation)
+                getProfiles(conversation.users)}}>
                 <ListItemAvatar>
                   <Avatar alt="Remy Sharp" src={require("../../assets/icons/paw.png").default} />
                 </ListItemAvatar>
@@ -70,35 +70,30 @@ const Chat = ({myProfile}) =>{
         </List>
       </Grid>
       <Grid item xs={9}>
-        <Container style={{height: "75vh"}}>
-          <Toolbar >
-            {/* <Typography > {conversationName} </Typography> */}
-          </Toolbar>
-          <List style={{height: '60vh', overflow: 'auto'}} >
-            {messages && userList && messages.slice(0).reverse().map(({id, user, created, text}) => {
-              var userInfo = userList.filter( userListItem => userListItem.username === user)[0]
-              var time = new Date(parseInt(created)).toLocaleString()
-              var output = user!==myProfile.username
-                ? <div key={id} style={{textAlign: "left"}} ref={messagesEndRef} >
-                    <img src={userInfo.profilePicture[0].uri} className="icon" alt="" onClick={() => history.push(`/user/${user}`)} style={{"borderRadius": "50%", "float": "left", "margin": "0 1rem"}} />
+        {userList && 
+          <Container style={{height: "75vh"}}>
+            <Toolbar >
+              <Typography > {conversation.conversationName} </Typography>
+            </Toolbar>
+            <List style={{height: '60vh', overflow: 'auto'}} >
+              {messages && messages.slice(0).reverse().map(({id, user, created, text}) => {
+                var userInfo = userList.filter( userListItem => userListItem.username === user)[0]
+                var time = new Date(parseInt(created)).toLocaleString()
+                return (
+                  <div key={id} style={{textAlign: user===myProfile.username?"right":"left"}} >
                     <p style={{marginBottom:"0.3rem"}}>{userInfo.firstName} {userInfo.lastName}</p>
-                    <Chip style={{fontSize:"0.9rem"}} color={user===myProfile.username?"primary": "secondary"} label={text} />
+                    <Chip style={{fontSize:"0.9rem"}} color={user===myProfile.username?"primary": "secondary"} label={text} ref={messagesEndRef} />
                     <p style={{fontSize: "0.5rem", marginBottom:"0.3rem"}}>{time}</p>
                   </div>
-                : <div key={id} style={{textAlign: "right"}} ref={messagesEndRef} >
-                    <img src={userInfo.profilePicture[0].uri} className="icon" alt="" style={{"borderRadius": "50%", "float": "right", "margin": "0 1rem"}} />
-                    <p style={{marginBottom:"0.3rem"}}>{userInfo.firstName} {userInfo.lastName}</p>
-                    <Chip style={{fontSize:"0.9rem"}} color={user===myProfile.username?"primary": "secondary"} label={text} ref={messagesEndRef}/>
-                    <p style={{fontSize: "0.5rem", marginBottom:"0.3rem"}}>{time}</p>
-                  </div>
-              return output
-            })}
-          </List>
-        </Container>
+                )
+              })}
+            </List>
+          </Container>
+        }
       </Grid>
       {conversation.conversationId && 
         <Grid item xs={12}>
-          <Container>
+          <Container >
             <Grid container spacing={2}>
               <Grid item xs={9}>
                 <TextField onChange={(e)=>{
